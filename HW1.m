5d5b58e27554064152abdd29b67857ce5e293275
ryantheengineer <ryan.larson.wa@gmail.com>
2019-01-15 20:20:39 -0700

    Got an optimum with less than the imposed limit of function calls.

--- HW1.m
+++ HW1.m
@@ -3,8 +3,8 @@ function [xopt, fopt, exitflag, output] = HW1()
     % ------------Starting point and bounds------------
     %var= d     D    n    hf    %design variables
     x0 = [0.05, 0.5, 10,  1.5];
-    ub = [0.2,  0.74,100, 20];
-    lb = [0.01, 0.55,1,   0.01];
+    ub = [0.2,  0.74,100,  20];
+    lb = [0.01, 0.5, 1,   0.01];
 
     % ------------Linear constraints------------
     A = [];
@@ -28,21 +28,25 @@ function [xopt, fopt, exitflag, output] = HW1()
         Se = 45000;     % psi
         w = 0.18;
         Sf = 1.5;
-        Q - 150000;     % psi
+        Q = 150000;     % psi
         
         
         % Analysis functions
         delta_max = (hf-h0) + delta0;
         delta_min = hf-h0;
+        hs = n*d;   % solid height
+        delta_hs = hf-hs;
         k = (G*d^4)/(8*D^3*n);  % spring stiffness
         F_preload = k*delta_min;   % preload force
         F_max = k*delta_max;       % max working force
+        F_hs = k*delta_hs;      % force at the solid height
         K = (4*D-d)/(4*(D-d)) + 0.62*d/D;   % Wahl factor
         tau_max = (8*F_max*D*K)/(pi*d^3);   % stress due to F_max
         tau_min = (8*F_preload*D*K)/(pi*d^3);   % stress due to F_preload
+        tau_hs = (8*F_hs*D*K)/(pi*d^3);
         tau_a = (tau_max - tau_min)/2;    % alternating stress
         tau_m = (tau_max + tau_min)/2;  % mean stress
-        hs = n*d;   % solid height
+        
         Sy = 0.44*Q/d^w;    % yield strength in shear
         
         
@@ -50,11 +54,16 @@ function [xopt, fopt, exitflag, output] = HW1()
         f = -F_preload;
         
         % Inequality constraints
-        c = zeros(4,1); % HOW MANY INEQUALITY CONSTRAINTS?
-        c(1) = stress_hs - Sy;  % stress at hs <= Sy
+        c = zeros(9,1); % HOW MANY INEQUALITY CONSTRAINTS?
+        c(1) = tau_hs - Sy;  % stress at hs <= Sy
         c(2) = D + d - 0.75;
         c(3) = d - 0.2;
-        c(4) = d + 0.01;    % not sure if this is the proper way to set this up
+        c(4) = 0.01 - d;    % not sure if this is the proper way to set this up
+        c(5) = (hf-delta_max) - hs - 0.05; % hs at least 0.05 in below lowest deflection point
+        c(6) = tau_a - (Se/Sf);
+        c(7) = tau_a + tau_m -(Sy/Sf);
+        c(8) = (D/d) - 16;
+        c(9) = 4 - (D/d);
         
         % Equality constraints
         ceq = [];
