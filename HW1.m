a939544af3954f506891f14e9a46428270d1e3a7
ryantheengineer <ryan.larson.wa@gmail.com>
2019-01-15 19:55:10 -0700

    Added analysis functions and some other stuff

--- HW1.m
+++ HW1.m
@@ -1,4 +1,4 @@
-function [xopt, fopt, exitflag, output] = optimize_template()
+function [xopt, fopt, exitflag, output] = HW1()
 
     % ------------Starting point and bounds------------
     %var= d     D    n    hf    %design variables
@@ -30,14 +30,31 @@ function [xopt, fopt, exitflag, output] = optimize_template()
         Sf = 1.5;
         Q - 150000;     % psi
         
+        
         % Analysis functions
+        delta_max = (hf-h0) + delta0;
+        delta_min = hf-h0;
+        k = (G*d^4)/(8*D^3*n);  % spring stiffness
+        F_preload = k*delta_min;   % preload force
+        F_max = k*delta_max;       % max working force
+        K = (4*D-d)/(4*(D-d)) + 0.62*d/D;   % Wahl factor
+        tau_max = (8*F_max*D*K)/(pi*d^3);   % stress due to F_max
+        tau_min = (8*F_preload*D*K)/(pi*d^3);   % stress due to F_preload
+        tau_a = (tau_max - tau_min)/2;    % alternating stress
+        tau_m = (tau_max + tau_min)/2;  % mean stress
+        hs = n*d;   % solid height
+        Sy = 0.44*Q/d^w;    % yield strength in shear
+        
         
         % Objective function
+        f = -F_preload;
         
         % Inequality constraints
-        c = zeros(3,1);
+        c = zeros(4,1); % HOW MANY INEQUALITY CONSTRAINTS?
         c(1) = stress_hs - Sy;  % stress at hs <= Sy
-        c(2) = 
+        c(2) = D + d - 0.75;
+        c(3) = d - 0.2;
+        c(4) = d + 0.01;    % not sure if this is the proper way to set this up
         
         % Equality constraints
         ceq = [];
