86a8c23dc0343de42fb45db75517fe536ce5f011
ryantheengineer <ryan.larson.wa@gmail.com>
2019-01-16 08:15:38 -0700

    Cleaned up equations and comments. clash and dsum are going right up to the limits so it might need some further constraint.

--- HW1.m
+++ HW1.m
@@ -22,52 +22,59 @@ function [xopt, fopt, exitflag, output] = HW1()
         hf = x(4);      % unloaded height
         
         % Other analysis variables (THESE ARE CORRECT)
-        h0 = 1.0;       % preload height
-        delta0 = 0.4;   % deflection
-        G = 12*10^6;    % psi
-        Se = 45000;     % psi
+        h0 = 1.0;               % preload height, in
+        delta0 = 0.4;           % deflection, in
+        hdef = h0 - delta0;     % deflection height, in
+        G = 12*10^6;            % psi
+        Se = 45000;             % psi
         w = 0.18;
         Sf = 1.5;
-        Q = 150000;     % psi
+        Q = 150000;             % psi
+        delta_f = 0;            % in
         
         
         % Analysis functions
-        hdef = h0 - delta0;
-        delta_max = (hf-h0) + delta0;
-        delta_min = hf-h0;
-        hs = n*d;   % solid height
-        delta_hs = hf-hs;
-        k = (G*d^4)/(8*(D^3)*n);    % spring stiffness
-        F_preload = k*delta_min;    % preload force
-        F_max = k*(hf-hdef);        % max working force
-%         F_max = k*delta_hs;        % max working force
-        F_s = k*delta_hs;          % force at the solid height
-        K = (4*D-d)/(4*(D-d)) + 0.62*d/D;   % Wahl factor
-        tau_max = (8*F_max*D*K)/(pi*d^3);   % stress due to F_max
-        tau_min = (8*F_preload*D*K)/(pi*d^3);   % stress due to F_preload
-%         tau_min = 0;
-        tau_hs = (8*F_s*D*K)/(pi*d^3);
-        tau_a = (tau_max - tau_min)/2;    % alternating stress
-        tau_m = (tau_max + tau_min)/2;  % mean stress
-        Sy = 0.44*Q/(d^w);    % yield strength in shear
-        clash = hdef - hs;
+        k = (G*d^4)/(8*(D^3)*n)            % spring stiffness
+        delta_p = hf-h0;                    % deflection at preload
+        delta_def = delta0 + (hf-h0);       % greatest working deflection
+        hs = n*d;                           % solid height
+        delta_s = hf - hs;                  % deflection at solid height
+        F_f = k*delta_f;                    % full height force (zero)
+        F_p = k*delta_p                    % preload force
+        F_def = k*delta_def;                % force at full deflection
+        F_s = k*delta_s;                    % force at solid height
+        K = (4*D-d)/(4*(D-d))+0.62*(d/D)   % Wahl factor
+        tau_f = ((8*F_f*D)/(pi*d^3))*K;     % stress at full height (zero)
+        tau_p = ((8*F_p*D)/(pi*d^3))*K;     % stress at preload height
+        tau_def = ((8*F_def*D)/(pi*d^3))*K; % stress at full deflection
+        tau_s = ((8*F_s*D)/(pi*d^3))*K;     % stress at solid height
+        tau_max = tau_def;                  % max stress (assumed at max deflection)
+        tau_min = tau_p;                    % min stress (assumed at preload deflection)
+        tau_m = (tau_max + tau_min)/2      % mean stress
+        tau_a = (tau_max - tau_min)/2      % alternating stress
+        Sy = 0.44*(Q/(d^w))                % yield stress
+        dratio = D/d;                       % ratio of diameters
+        dsum = D + d                       % sum of diameters
+        clash = hdef - hs                  % clash allowance
+        Sefratio = Se/Sf;                   % endurance limit to safety factor
+        Syfratio = Sy/Sf;                   % yield strength to safety factor
+        tau_amsum = tau_a + tau_m;          % sum of alternating and mean stresses
         
         % Objective function
-        f = -F_preload;
+        f = -F_p;                           % Maximize the preload force
         
         % Inequality constraints
         c = zeros(9,1);
-        c(1) = tau_hs - Sy;             % stress at hs <= Sy
-        c(2) = D + d - 0.75;            % D + d <= 0.75
-        c(3) = d - 0.2;                 % d <= 0.2
-        c(4) = 0.01 - d;                % 0.01 <= d
-        c(5) = 0.05 - clash;            % 0.05 <= clash
-        c(6) = tau_a - (Se/Sf);         % tau_a <= Se/Sf
-        c(7) = tau_a + tau_m -(Sy/Sf);  % tau_a + tau_m <= Sy/Sf
-        c(8) = (D/d) - 16;
-        c(9) = 4 - (D/d);
+        c(1) = tau_a - Sefratio;            % tau_a <= Se/Sf
+        c(2) = tau_amsum - Syfratio;        % tau_a + tau_m <= Sy/Sf
+        c(3) = dratio - 16;                 % D/d <= 16
+        c(4) = 4 - dratio;                  % 4 <= D/d
+        c(5) = d - 0.2;                     % d <= 0.2
+        c(6) = 0.01 - d;                    % 0.01 <= d
+        c(7) = dsum - 0.75;                 % D + d <= 0.75
+        c(8) = 0.05 - clash;                % 0.05 <= clash allowance
+        c(9) = tau_s - Sy;                  % tau_s <= Sy
         
-        Ddsum = D + d;   % Right now, the algorithm is taking this constraint right up to the boundary of 0.75
         % Equality constraints
         ceq = [];
         
