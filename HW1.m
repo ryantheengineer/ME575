822acac12d3b4e95bd1a1e9b825a6440859b24da
ryantheengineer <ryan.larson.wa@gmail.com>
2019-01-17 08:23:38 -0700

    Minor changes after meeting with professor

--- HW1.m
+++ HW1.m
@@ -4,7 +4,7 @@ function [xopt, fopt, exitflag, output] = HW1()
     %var= d      D      n      hf           % Design variables
     x0 = [0.050, 0.500, 10.00, 1.500];      % Starting point
     ub = [0.250, 1.000, 100.0, 20.00];      % Upper bound
-    lb = [0.001, 0.200, 1.000, 0.010];      % Lower bound
+    lb = [0.010, 0.200, 1.000, 0.010];      % Lower bound
 
     % ------------Linear constraints------------
     A = [];
@@ -33,28 +33,28 @@ function [xopt, fopt, exitflag, output] = HW1()
         delta_f = 0;            % in
         
         % Analysis functions
-        k = (G*d^4)/(8*(D^3)*n)            % spring stiffness
+        k = (G*d^4)/(8*(D^3)*n);            % spring stiffness
         delta_p = hf-h0;                    % deflection at preload
         delta_def = delta0 + (hf-h0);       % greatest working deflection
         hs = n*d;                           % solid height
         delta_s = hf - hs;                  % deflection at solid height
         F_f = k*delta_f;                    % full height force (zero)
-        F_p = k*delta_p                    % preload force
+        F_p = k*delta_p;                    % preload force
         F_def = k*delta_def;                % force at full deflection
         F_s = k*delta_s;                    % force at solid height
-        K = ((4*D)-d)/(4*(D-d))+0.62*(d/D)   % Wahl factor
+        K = ((4*D)-d)/(4*(D-d))+0.62*(d/D); % Wahl factor
         tau_f = ((8*F_f*D)/(pi*d^3))*K;     % stress at full height (zero)
         tau_p = ((8*F_p*D)/(pi*d^3))*K;     % stress at preload height
         tau_def = ((8*F_def*D)/(pi*d^3))*K; % stress at full deflection
         tau_s = ((8*F_s*D)/(pi*d^3))*K;     % stress at solid height
         tau_max = tau_def;                  % max stress (assumed at max deflection)
         tau_min = tau_p;                    % min stress (assumed at preload deflection)
-        tau_m = (tau_max + tau_min)/2      % mean stress
-        tau_a = (tau_max - tau_min)/2      % alternating stress
-        Sy = 0.44*(Q/(d^w))                % yield stress
+        tau_m = (tau_max + tau_min)/2;      % mean stress
+        tau_a = (tau_max - tau_min)/2;      % alternating stress
+        Sy = 0.44*(Q/(d^w));                % yield stress
         dratio = D/d;                       % ratio of diameters
-        dsum = D + d                       % sum of diameters
-        clash = hdef - hs                  % clash allowance
+        dsum = D + d;                       % sum of diameters
+        clash = hdef - hs;                  % clash allowance
         Sefratio = Se/Sf;                   % endurance limit to safety factor
         Syfratio = Sy/Sf;                   % yield strength to safety factor
         tau_amsum = tau_a + tau_m;          % sum of alternating and mean stresses
@@ -82,7 +82,10 @@ function [xopt, fopt, exitflag, output] = HW1()
     % ------------Call fmincon------------
     options = optimoptions(@fmincon, 'display', 'iter-detailed');
     [xopt, fopt, exitflag, output] = fmincon(@obj, x0, A, b, Aeq, beq, lb, ub, @con, options);
-    
+    xopt
+    fopt
+    [~,c,~] = objcon(xopt);
+    c
     
     % ------------Separate obj/con (do not change)------------
     function [f] = obj(x)
