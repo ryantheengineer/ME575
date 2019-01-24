9e0b814e2430ace6fee791850fb03d2ab2ea230f
ryantheengineer <ryan.larson.wa@gmail.com>
2019-01-24 13:43:58 -0700

    Fixed incorrect interpolation that was throwing the whole result off.

--- HW2.m
+++ HW2.m
@@ -2,7 +2,7 @@ function [xopt, fopt, exitflag, output] = HW2()
 
     % ------------Starting point and bounds------------
     %var= V     D      d            % Design variables
-    x0 = [14.96,   0.4,  0.005];
+    x0 = [14.96,0.4,   0.005];
     ub = [30,   0.5,   0.01];
     lb = [0.01, 0.1,   0.0005];
 
@@ -30,23 +30,22 @@ function [xopt, fopt, exitflag, output] = HW2()
         S = gamma/rho_w;    % limestone specific gravity
         mu = 7.392*10^-4;   % viscosity of water lbm/(ft*s)
         gc = 32.17;         % conversion factor between lbf and lbm
-        fw = 0.02;
         
         % Analysis functions
-        C = 4*W/(pi*gamma*V*D^2);
+        C = 4*W/(pi*gamma*V*(D^2));
         Area = (pi/4)*D^2;
         rho = rho_w + C*(gamma-rho_w);
-        Pg = 218*W*((1/sqrt(d)) - (1/sqrt(a)));
+        Pg = (218*W*((1/sqrt(d)) - (1/sqrt(a))))/550;
         CdRpsq_calculated = 4*g*rho_w*(d^3)*((gamma-rho_w)/(3*mu^2));
         Cd = dragReynolds(CdRpsq_calculated);
         Rw = (rho_w*V*D)/mu;
-%         fw = fw_function(Rw);
+        fw = fw_function(Rw);
         F = fw*((rho_w/rho) + 150*C*(rho_w/rho)*((g*D*(S-1))/((V^2)*sqrt(Cd)))^1.5);
         delta_p = (F*rho*L*V^2)/(2*D*gc);    % 
         Qslurry = Area*V;
-        Pf = delta_p*Qslurry;
+        Pf = (delta_p*Qslurry)/550;     % pump power, hp
         Vc = ((40*g*C*(S-1)*D)/sqrt(Cd))^0.5;
-        horsepower = (Pf + Pg)/550;
+        horsepower = Pf + Pg;
         cost = NPV(horsepower,0.07,7); % develop cost function here
         
         % Objective function
@@ -55,11 +54,7 @@ function [xopt, fopt, exitflag, output] = HW2()
         % Inequality constraints
         c = zeros(2,1);
         c(1) = (1.1*Vc) - V;        % 1.1*Vc <= V
-        c(2) = C - 0.4;             % C <= 0.4
-%         c(3) = 1 - horsepower;      % horsepower >= 1
-%         c(4) = 0 - Pf;              % Pf >= 0
-%         c(5) = 0 - Pg;              % Pg >= 0
-        
+        c(2) = C - 0.4;             % C <= 0.4        
         
         % Equality constraints
         ceq = [];
--- dragReynolds.m
+++ dragReynolds.m
@@ -4,7 +4,7 @@ function [Cd_interpolated] = dragReynolds(CdRpsq_calculated)
 
 load('Cd_CdRp_sq_correlation.mat','Cd_vector','CdRpsq');
 
-Cd_interpolated = interp1(Cd_vector,CdRpsq,CdRpsq_calculated,'pchip');
+Cd_interpolated = interp1(CdRpsq,Cd_vector,CdRpsq_calculated,'pchip');
 
 end
 
