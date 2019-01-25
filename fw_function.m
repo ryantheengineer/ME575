bb376db05de9eb90be8980ebccda3a9c794b1461
ryantheengineer <ryan.larson.wa@gmail.com>
2019-01-25 07:38:59 -0700

    Fixed some things for contour plot

--- fw_function.m
+++ fw_function.m
@@ -2,9 +2,9 @@ function [fw] = fw_function(Rw)
 %fw_function: Calculates the value of fw based on a given Rw
 
 if Rw <= 10^5
-    fw = 0.3164/(Rw^0.25);
+    fw = 0.3164/(Rw.^0.25);
 else
-    fw = 0.0032 + 0.221*Rw^(-0.237);
+    fw = 0.0032 + 0.221*Rw.^(-0.237);
 end
 
 end
