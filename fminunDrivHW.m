60158feb5f937f9c487e845f3ae900a8cdbf8d3b
ryantheengineer <ryan.larson.wa@gmail.com>
2019-02-01 10:31:45 -0700

    Added fminun.m and fminunDrivHW.m, from class content

---
+++ fminunDrivHW.m
@@ -0,0 +1,38 @@
+%---Example Driver program for fminun
+    clear;
+
+    global nobj ngrad
+    nobj = 0; % counter for objective evaluations
+    ngrad = 0.; % counter for gradient evaluations
+    x0 = [1; 1]; % starting point, set to be column vector
+    algoflag = 1; % 1=steepest descent; 2=BFGS quasi-Newton
+    stoptol = 1.e-3; % stopping tolerance, all gradient elements must be < stoptol  
+    
+    
+    % ---------- call fminun----------------
+    [xopt, fopt, exitflag] = fminun(@obj, @gradobj, x0, stoptol, algoflag);
+   
+    xopt
+    fopt
+    nobj
+    ngrad
+    
+   
+     % function to be minimized
+     function [f] = obj(x)
+        global nobj
+        %example function
+        f = 12 + 6*x(1) - 5*x(2) + 4*x(1)^2 -2*x(1)*x(2) + 6*x(2)^2;
+        nobj = nobj +1;
+     end
+     
+    % get gradient as a column vector
+     function [grad] = gradobj(x)
+        global ngrad
+        %gradient for function 1
+        grad(1,1) = 6 + 8*x(1) - 2*x(2);
+        grad(2,1) = -5 - 2*x(1) + 12*x(2);
+        ngrad = ngrad + 1;
+     end
+     
+    
